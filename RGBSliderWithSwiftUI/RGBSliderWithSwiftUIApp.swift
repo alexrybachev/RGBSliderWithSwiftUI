b29ec50cbc0bae05b6178e7a55d5d1ffade857ba
Alex Rybachev <rybachev.as@gmail.com>
2022-05-16 18:22:11 +0400

    Initial Commit

---
+++ RGBSliderWithSwiftUI/RGBSliderWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBSliderWithSwiftUIApp.swift
+//  RGBSliderWithSwiftUI
+//
+//  Created by Aleksandr Rybachev on 16.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct RGBSliderWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
